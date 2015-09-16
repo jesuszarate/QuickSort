bd475eb875f99dcc082ce580d25009083bf85da4
Jesus <chuy8jay@gmail.com>
2015-09-15 22:30:56 -0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Jesus Zarate on 9/15/15.
+ */
+public class Main
+{
+    public static void main(String args[])
+    {
+
+    }
+}
---
+++ src/QuickSort/QuickSort.java
@@ -0,0 +1,12 @@
+package QuickSort;
+
+/**
+ * Created by Jesus Zarate on 9/15/15.
+ *
+ * Implementation of QuickSort
+ */
+public class QuickSort
+{
+    
+
+}
